5b8389b0c8c0049fa625e7596ec491df99458421
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-01-19 11:16:51 +0100

    💥 MVC STRUCTURE

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <?php
 require_once 'Personnage.php';
-
-$Gandalf = new Personnage("Gandalf", 100, 500);
-$Sauron = new Personnage("Sauron", 0, 1000);
-
-var_dump($Gandalf);
-var_dump($Sauron);
-
-echo "Gandalf attaque de ".$Gandalf->getAtk()." et a actuellement ".$Gandalf->getPv()." pv <br>";
-echo $Gandalf->crier();
-
-$Gandalf->regenerer(30);
+session_start();
 
 
+if (!isset($_SESSION['perso1']) && !isset($_SESSION['perso2'])) {
+    $Gandalf = new Personnage("Gandalf", 100, 500);
+    $_SESSION['perso1'] = $Gandalf;
 
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
-echo $Gandalf->attaque($Sauron);
+    $Sauron = new Personnage("Sauron", 0, 1000);
+    $_SESSION['perso2'] = $Sauron;
+}
+
+var_dump($_SESSION['perso1']);
+var_dump($_SESSION['perso2']);
+
+echo "Gandalf attaque de ".$_SESSION['perso1']->getAtk()." et a actuellement ".$_SESSION['perso1']->getPv()." pv <br>";
+echo $_SESSION['perso1']->crier();
+
+// $_SESSION['perso1']->regenerer(30);
+
+if (isset($_GET['atk'])) {
+    echo $_SESSION['perso1']->attaque($_SESSION['perso2']);
+} else if (isset($_GET['restart'])) {
+    $_SESSION = array();
+    session_destroy();
+    header('location:index.php');
+}
 
 
-$Sauron->regenerer(30);
+
+echo "<a href='index.php?atk'>Attaquer</a>\n";
+echo "<a href='index.php?restart'>Recommencer</a>\n";
+
+
+// $_SESSION['perso2']->regenerer(30);
 ?>
