f041d85c3b14dea46023004de1bd4aea8de023f3
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-01-22 11:40:03 +0100

    ✨ Choix perso

---
+++ choixPerso.php
@@ -0,0 +1,42 @@
+<h1>Choisissez vos personnages</h1>
+<form action='index.php' method='post'>
+    <div>
+        <h2>Personnage 1</h2>
+        <div>
+            <input type="radio" id="Gandalf1" name="perso1" value="Gandalf" checked />
+            <label for="Gandalf1">Gandalf</label>
+        </div>
+        <div>
+            <input type="radio" id="Sauron1" name="perso1" value="Sauron" />
+            <label for="Sauron1">Sauron</label>
+        </div>
+        <div>
+            <input type="radio" id="Caro1" name="perso1" value="Caro" />
+            <label for="Caro1">Caro</label>
+        </div>
+        <div>
+            <input type="radio" id="Morgan1" name="perso1" value="Morgan" />
+            <label for="Morgan1">Morgan</label>
+        </div>
+    </div>
+    <div>
+        <h2>Personnage 2</h2>
+        <div>
+            <input type="radio" id="Gandalf2" name="perso2" value="Gandalf" checked />
+            <label for="Gandalf2">Gandalf</label>
+        </div>
+        <div>
+            <input type="radio" id="Sauron2" name="perso2" value="Sauron" />
+            <label for="Sauron2">Sauron</label>
+        </div>
+        <div>
+            <input type="radio" id="Caro2" name="perso2" value="Caro" />
+            <label for="Caro2">Caro</label>
+        </div>
+        <div>
+            <input type="radio" id="Morgan2" name="perso2" value="Morgan" />
+            <label for="Morgan2">Morgan</label>
+        </div>
+    </div>
+    <input type="submit" value="FIGHT !"/>
+</form>
--- index.php
+++ index.php
@@ -1,37 +1,66 @@
 <?php
 require_once 'Personnage.php';
 session_start();
-
-
-if (!isset($_SESSION['perso1']) && !isset($_SESSION['perso2'])) {
-    $Gandalf = new Personnage("Gandalf", 100, 500);
-    $_SESSION['perso1'] = $Gandalf;
-
-    $Sauron = new Personnage("Sauron", 0, 1000);
-    $_SESSION['perso2'] = $Sauron;
-}
-
-var_dump($_SESSION['perso1']);
-var_dump($_SESSION['perso2']);
-
-echo "Gandalf attaque de ".$_SESSION['perso1']->getAtk()." et a actuellement ".$_SESSION['perso1']->getPv()." pv <br>";
-echo $_SESSION['perso1']->crier();
-
-// $_SESSION['perso1']->regenerer(30);
-
-if (isset($_GET['atk'])) {
-    echo $_SESSION['perso1']->attaque($_SESSION['perso2']);
-} else if (isset($_GET['restart'])) {
-    $_SESSION = array();
-    session_destroy();
-    header('location:index.php');
-}
-
-
-
-echo "<a href='index.php?atk'>Attaquer</a>\n";
-echo "<a href='index.php?restart'>Recommencer</a>\n";
-
-
-// $_SESSION['perso2']->regenerer(30);
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>MMI FIGHT</title>
+        <link rel="stylesheet" type="text/css" href="style/style.css"/>
+        <script type="text/javascript" src="js/script.js"></script>
+    </head>
+    <body>
+<?php
+    if (!isset($_SESSION['perso1']) && !isset($_SESSION['perso2'])) {
+    if (!isset($_POST['perso1']) && !isset($_POST['perso2'])) {
+        require_once 'choixPerso.php';
+    } else {
+        $_SESSION['perso1'] = new Personnage($_POST['perso1'], 100, 500);
+    
+        $_SESSION['perso2'] = new Personnage($_POST['perso2'], 0, 1000);
+
+        header('location:index.php');
+    }
+    
+} else {
+    var_dump($_SESSION['perso1']);
+    var_dump($_SESSION['perso2']);
+    
+    echo $_SESSION['perso1']->getName()." attaque de ".$_SESSION['perso1']->getAtk()." et a actuellement ".$_SESSION['perso1']->getPv()." pv <br>";
+    echo $_SESSION['perso1']->crier();
+    
+    // $_SESSION['perso1']->regenerer(30);
+    
+    if (isset($_GET['atk'])) {
+        echo $_SESSION['perso1']->attaque($_SESSION['perso2']);
+    } else if (isset($_GET['restart'])) {
+        $_SESSION = array();
+        session_destroy();
+        header('location:index.php');
+    }
+    
+    
+    
+    echo "<a href='index.php?atk'>Attaquer</a>\n";
+    echo "<a href='index.php?restart'>Recommencer</a>\n";
+    
+    
+    // $_SESSION['perso2']->regenerer(30);
+}
+
+
+?>
+    </body>
+</html>
+
+<!-- 
+     _____     _____
+    |  _  |   |  _  |
+   -| | | |---| | | |-
+    |_____| 7 |_____|  ~B!nro~
+    
+-->
+
